297cf7f2bd3386f1b28a0c0629e861fb7875e388
DevChan775 <leeyuch6@naver.com>
2026-08-27 11:19:38 +0900

    refactor: 코드 주석 수정 및 정리

--- backend/data_setup.py
+++ backend/data_setup.py
@@ -3,19 +3,19 @@ import numpy as np
 
 def process_web_image(file_bytes):
     """
-    웹에서 전달받은 바이너리 데이터를 하드디스크 저장 없이 
-    즉시 OpenCV 이미지 배열로 변환
+    웹에서 전달받은 이미지 바이너리 데이터를 디스크 저장(I/O) 과정 없이 
+    메모리 상의 OpenCV 배열(NumPy)로 디코딩을 진행
     """
-    # 0과 1로 이루어진 웹 데이터를 파이썬이 읽을 수 있는 배열로 변환
     nparr = np.frombuffer(file_bytes, np.uint8)
-
-    # 배열을 OpenCV 이미지로 해독
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
     if img is None:
         return None, "⚠️ 에러: 이미지를 해독할 수 없습니다."
     
-    return img, "SUCCESS"
+    return img
+
+
+
 
 
 
